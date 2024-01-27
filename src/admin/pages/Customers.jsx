8e6282bb482b41fa485fc6040ad262e2b392d3e6
Kamran Dilshad <rana.kamran141947@gmail.com>
2024-01-27 12:57:28 +0500

    customer changes

--- src/admin/pages/Customers.jsx
+++ src/admin/pages/Customers.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
 	GridComponent,
 	ColumnsDirective,
@@ -14,11 +14,22 @@ import {
 
 import { customersData, customersGrid } from '../data/dummy';
 import { Header } from '../components';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllTrainee } from '../../redux/actions/TraineesAction';
 
 const Customers = () => {
+	const dispatch = useDispatch();
 	const selectionsettings = { persistSelection: true };
 	const toolbarOptions = ['Delete'];
 	const editing = { allowDeleting: true, allowEditing: true };
+
+	const allTrainees = useSelector(
+		(state) => state?.traineeRegister?.traineeRegister
+	);
+
+	useEffect(() => {
+		dispatch(getAllTrainee());
+	}, []);
 
 	return (
 		<div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
